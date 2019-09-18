01eb7a52363a22be8240f051453c14144e1196a4
ꜱᴩʀɪᴛᴇ➀ <s.1@null.net>
2019-09-18 11:04:20 +0800

    Initial commit.

---
+++ background.js
@@ -0,0 +1,27 @@
+chrome.webRequest.onBeforeRequest.addListener(
+	function(event) {
+		var path = event.url
+		var tmp = document.createElement('a');
+		tmp.href = path;
+
+		if (tmp.hostname == "z.net") {
+			path = path.substr(13, path.length);
+		} else {
+			if (path.startsWith("https://")) {
+				path = path.substr(8,path.length);
+			}
+			if (path.startsWith("http://")) {
+				path = path.substr(7,path.length);
+			}
+		}
+
+		chrome.tabs.update(event.tabId, {url: "http://127.0.0.1:43110/" + path});
+		return {cancel: true};
+	},
+	{urls: ["<all_urls>"]},
+	["blocking"]
+);
+
+chrome.omnibox.onInputEntered.addListener(function(text){
+	chrome.tabs.update({url: "http://127.0.0.1:43110/" + text});
+});
